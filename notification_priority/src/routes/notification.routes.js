7331b122b2c300bbd723f4847fc8dcc5a5d9bb5a
rdineshkumarreddy <dineshkumarreddy58@gmail.com>
2026-05-30 13:21:03 +0530

    Delete notification_priority directory

--- notification_priority/src/routes/notification.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const notificationController = require('../controllers/notification.controller');
-
-const router = express.Router();
-
-router.get('/top-notifications', notificationController.getTopNotifications);
-
-module.exports = router;
